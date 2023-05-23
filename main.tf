8544e0133ad3e8b71721694043a419d199b1a34b
abhishek choudharry <abhishek.choudharry@gmail.com>
2023-05-23 19:02:07 +0530

    Fixed the CIDR values for subnet

--- main.tf
+++ main.tf
@@ -32,19 +32,19 @@ resource "azurerm_virtual_network" "virtualnetowrk" {
 
   subnet {
     name           = "South"
-    address_prefix = "192.168.0.24/28"
+    address_prefix = "192.168.0.16/28"
     security_group = azurerm_network_security_group.nsg.id
   }
 
   subnet {
     name           = "East"
-    address_prefix = "192.168.0.54/28"
+    address_prefix = "192.168.0.48/28"
     security_group = azurerm_network_security_group.nsg.id
   }
 
   subnet {
     name           = "West"
-    address_prefix = "192.168.0.94/28"
+    address_prefix = "192.168.0.96/28"
     security_group = azurerm_network_security_group.nsg.id
   }
   tags = {
