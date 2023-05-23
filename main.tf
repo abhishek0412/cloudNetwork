ec90a99da0b4a2d4e0df1d4c196dd11d0df4c173
abhishek choudharry <abhishek.choudharry@gmail.com>
2023-05-23 18:53:28 +0530

    Fixed the variables to dependent output variables from script itself

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_network_security_group" "nsg" {
   name                = var.nsgname
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   tags = {
     purpose = var.tagname
   }
@@ -21,7 +21,7 @@ resource "azurerm_network_security_group" "nsg" {
 resource "azurerm_virtual_network" "virtualnetowrk" {
   name                = var.vnet_name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["192.168.0.0/24"]
 
   subnet {
