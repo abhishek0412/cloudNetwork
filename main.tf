c0d1802e6ac0c5d105c9c1cf441bf03ba281c793
abhishek choudharry <abhishek.choudharry@gmail.com>
2023-05-23 18:19:08 +0530

    Added the network security group , vnet & subnet

--- main.tf
+++ main.tf
@@ -1,4 +1,44 @@
+# A resource Group
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
+}
+
+# A Network security group
+resource "azurerm_network_security_group" "network_security" {
+  name                = "network_security"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+}
+
+# A virtual network
+resource "azurerm_virtual_network" "myvn" {
+  name                = "IndiaVnet"
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  address_space       = ["192.168.0.0/24"]
+
+  subnet {
+    name           = "North"
+    address_prefix = "192.168.0.0/28"
+    security_group = azurerm_network_security_group.network_security.id
+  }
+
+  subnet {
+    name           = "South"
+    address_prefix = "192.168.0.24/28"
+    security_group = azurerm_network_security_group.network_security.id
+  }
+
+  subnet {
+    name           = "East"
+    address_prefix = "192.168.0.54/28"
+    security_group = azurerm_network_security_group.network_security.id
+  }
+
+  subnet {
+    name           = "West"
+    address_prefix = "192.168.0.94/28"
+    security_group = azurerm_network_security_group.network_security.id
+  }
 }
