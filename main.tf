fc005adc58944383cb1c976f6ad4f73b63bd39e5
abhishek choudharry <abhishek.choudharry@gmail.com>
2023-05-23 18:41:39 +0530

    updated the hardcode values with variables

--- main.tf
+++ main.tf
@@ -1,44 +1,53 @@
 # A resource Group
 resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
+  location = var.location
   name     = var.resource_group_name
+  tags = {
+    purpose = var.tagname
+  }
 }
 
 # A Network security group
-resource "azurerm_network_security_group" "network_security" {
-  name                = "network_security"
+resource "azurerm_network_security_group" "nsg" {
+  name                = var.nsgname
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
+  tags = {
+    purpose = var.tagname
+  }
 }
 
 # A virtual network
-resource "azurerm_virtual_network" "myvn" {
-  name                = "IndiaVnet"
-  location            = var.resource_group_location
+resource "azurerm_virtual_network" "virtualnetowrk" {
+  name                = var.vnet_name
+  location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = ["192.168.0.0/24"]
 
   subnet {
     name           = "North"
     address_prefix = "192.168.0.0/28"
-    security_group = azurerm_network_security_group.network_security.id
+    security_group = azurerm_network_security_group.nsg.id
   }
 
   subnet {
     name           = "South"
     address_prefix = "192.168.0.24/28"
-    security_group = azurerm_network_security_group.network_security.id
+    security_group = azurerm_network_security_group.nsg.id
   }
 
   subnet {
     name           = "East"
     address_prefix = "192.168.0.54/28"
-    security_group = azurerm_network_security_group.network_security.id
+    security_group = azurerm_network_security_group.nsg.id
   }
 
   subnet {
     name           = "West"
     address_prefix = "192.168.0.94/28"
-    security_group = azurerm_network_security_group.network_security.id
+    security_group = azurerm_network_security_group.nsg.id
+  }
+  tags = {
+    purpose = var.tagname
   }
 }
